1030496b926e8d8373026b360aa3fe756723bb45
Adrian Miarczyński <adrian331144@gmail.com>
2023-03-19 21:06:30 +0100

    finish nine tasks

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,12 @@
+const btnColor = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnColor.addEventListener("click", () => {
+  colorSpan.innerHTML = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});

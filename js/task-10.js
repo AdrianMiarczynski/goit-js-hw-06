83b85c844d9b0c3b081498ae6ab471cd5f1f9b38
Adrian Miarczyński <adrian331144@gmail.com>
2023-03-20 14:55:53 +0100

    finish all tasks

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,41 @@
+const createBtn = document.querySelector("button[data-create]");
+const deleteBtn = document.querySelector("button[data-destroy]");
+const box = document.querySelector("#boxes");
+let inputEl = document.querySelector("input[type=number]");
+console.log(inputEl);
+
+//generowanie koloru
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+// wartość początkowa i zmienne
+const divFirstValue = 30;
+let divValue = 30;
+let divSize = 10;
+
+//
+
+// tworzenie obiektu z losowym kolorem
+const createObject = () => {
+  let inputValue = inputEl.value;
+  for (let i = 0; i < inputValue; i++) {
+    let divEl = document.createElement("div");
+    divEl.style.width = `${divValue}px`;
+    divEl.style.height = `${divValue}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    box.append(divEl);
+    divValue += divSize;
+  }
+};``
+
+// usuwanie divów
+const delateObject = () => {
+  // box.remove();
+  box.textContent = "";
+};
+
+// wywoływanie funkcji
+createBtn.addEventListener("click", createObject);
+deleteBtn.addEventListener("click", delateObject);
